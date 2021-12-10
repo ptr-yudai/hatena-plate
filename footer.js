764155044c12e1da5e79a859b02884bc7f0b546b
Yudai Fujiwara <ptr.yudai+dev@gmail.com>
2021-12-10 17:59:25 +0900

    Preserve empty line

--- footer.js
+++ footer.js
@@ -8,9 +8,7 @@ var codeBlocks = document.getElementsByClassName('code');
     var lines = e.innerHTML.split(/\n/);
     var codeBlock = "";
     lines.forEach(function(line){
-        if(line != ""){
-            codeBlock += '<div class="code-line">' + line + '</div>'      
-        }
+        codeBlock += '<div class="code-line">' + line + '</div>';
     })
     e.innerHTML = codeBlock;
     e.classList.add("code-lang");
